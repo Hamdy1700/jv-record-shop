e255f067ffcacd2fbd95245141b67888d3048a42
Hamdy Abdalla <ha17007001@gmail.com>
2024-12-05 10:04:23 +0000

    first commit

---
+++ src/main/java/com/record_shop_backend/RecordShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.record_shop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/record_shop_backend/RecordShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.record_shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
