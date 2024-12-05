9ef858d1c71562d713c221f3ee3d7d98a4db42d6
Hamdy Abdalla <ha17007001@gmail.com>
2024-12-05 10:34:04 +0000

    feat: add Genre

---
+++ src/main/java/com/record_shop_backend/model/Genre.java
@@ -0,0 +1,16 @@
+package com.record_shop_backend.model;
+
+public enum Genre {
+    POP,
+    ROCK,
+    HIP_HOP,
+    RB,
+    EDM,
+    COUNTRY,
+    JAZZ,
+    REGGAE,
+    CLASSICAL,
+    LATIN,
+    METAL,
+    SOUL
+}
