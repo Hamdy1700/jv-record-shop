48b52e0bbe68b6c40ec79c843661ac5821e5981a
Hamdy Abdalla <ha17007001@gmail.com>
2024-12-06 14:22:33 +0000

    feat: add get all albums tests for controller and service and get album by id tests for controller and service

--- src/test/java/com/record_shop_backend/controller/RecordShopControllerTest.java
+++ src/test/java/com/record_shop_backend/controller/RecordShopControllerTest.java
@@ -1,14 +1,26 @@
 package com.record_shop_backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.record_shop_backend.model.Album;
+import com.record_shop_backend.model.Genre;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import com.record_shop_backend.service.AlbumService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -29,6 +41,44 @@ public class RecordShopControllerTest {
     public void setup() {
         mockMvcController = MockMvcBuilders.standaloneSetup(albumController).build();
         mapper = new ObjectMapper();
+    }
+
+    @Test
+    @DisplayName("Get all albums")
+    void testGetAllAlbums() throws Exception {
+        List<Album> albums = new ArrayList<>();
+
+        albums.add(new Album(1L, "album1", "artist1", Genre.POP, 1994));
+        albums.add(new Album(2L, "album2", "artist2", Genre.ROCK, 2004));
+        albums.add(new Album(3L, "album3", "artist3", Genre.METAL, 2008));
+
+        when(mockAlbumService.getAllAlbums()).thenReturn(albums);
+
+        this.mockMvcController.perform(
+                MockMvcRequestBuilders.get("/api/v1/albums"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("album1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("album2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].id").value(3))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].name").value("album3"));
+    }
+
+    @Test
+    @DisplayName("Get album by id")
+    void testGetAlbumById() throws Exception {
+        Long id = 1L;
+        Album album = new Album(1L, "album1", "artist1", Genre.POP, 1994);
+
+        when(mockAlbumService.getAlbumById(id)).thenReturn(album);
+
+        this.mockMvcController.perform(
+                MockMvcRequestBuilders.get("/api/v1/albums/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("album1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.artist").value("artist1"));
     }
 
 }
--- src/test/java/com/record_shop_backend/service/RecordShopServiceTest.java
+++ src/test/java/com/record_shop_backend/service/RecordShopServiceTest.java
@@ -1,10 +1,20 @@
 package com.record_shop_backend.service;
 
+import com.record_shop_backend.model.Album;
+import com.record_shop_backend.model.Genre;
 import com.record_shop_backend.repository.AlbumRepository;
-import jakarta.inject.Inject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @DataJpaTest
 public class RecordShopServiceTest {
@@ -13,6 +23,36 @@ public class RecordShopServiceTest {
     private AlbumRepository mockAlbumRepository;
 
     @InjectMocks
-    private AlbumService albumService;
+    private AlbumServiceImpl albumServiceImpl;
+
+    @Test
+    @DisplayName("GET all albums responds with all albums")
+    void testGetAllAlbums() {
+        List<Album> albums = new ArrayList<>();
+
+        albums.add(new Album(1L, "album1", "artist1", Genre.POP, 1994));
+        albums.add(new Album(2L, "album2", "artist2", Genre.ROCK, 2004));
+        albums.add(new Album(3L, "album3", "artist3", Genre.METAL, 2008));
+
+        when(mockAlbumRepository.findAll()).thenReturn(albums);
+
+        List<Album> result = albumServiceImpl.getAllAlbums();
+
+        assertThat(result).hasSize(3);
+        assertThat(result).isEqualTo(albums);
+    }
+
+    @Test
+    @DisplayName("Get album by Id responds with album of given id")
+    void testGetAlbumById() {
+        Long id = 1L;
+        Optional<Album> album = Optional.of(new Album(1L, "album1", "artist1", Genre.POP, 1994));
+
+        when(mockAlbumRepository.findById(id)).thenReturn(album);
+
+        Album result = albumServiceImpl.getAlbumById(id);
+
+        assertThat(result).isEqualTo(album.get());
+    }
 
 }
